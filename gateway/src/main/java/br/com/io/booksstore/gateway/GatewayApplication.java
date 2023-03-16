ae778815c0f6baf54e07f3451e495e3c4bb93257
João Oliveira <joaoholiveira1@hotmail.com>
2023-03-16 17:08:45 -0300

    Adding clients health endpoint and testing it through gateway microservice and updating the clients microservice name.

--- gateway/src/main/java/br/com/io/booksstore/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/com/io/booksstore/gateway/GatewayApplication.java
@@ -3,7 +3,10 @@ package br.com.io.booksstore.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +15,14 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/clients/**").uri("lb://clients-microservice"))
+				.build();
 	}
 
 }
